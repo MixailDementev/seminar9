1fe69f3784cdd83feb4ee4fe1a537e0f74cba0ac
MDementev <vanneste1207@gmail.com>
2022-12-01 18:52:34 +0300

    добавил кнопки

--- bot.py
+++ bot.py
@@ -51,7 +51,6 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Displays info on how to use the bot."""
     await update.message.reply_text("Use /start to test this bot.")
 
 
